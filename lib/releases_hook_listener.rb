8775004da6a1f9c031d13acab7d12001893a519c
Greg Sutcliffe <greg.sutcliffe@gmail.com>
2018-06-18 14:47:07 +0100

    change to use Version: update controller save hook

--- lib/releases_hook_listener.rb
+++ lib/releases_hook_listener.rb
@@ -1,19 +1,30 @@
 class ReleasesHookListener < Redmine::Hook::ViewListener
+  # View Hooks to add fields to Issue#show and Issue#edit
   render_on :view_issues_show_details_bottom,
     :partial => 'hooks/show_attributes'
 
   render_on :view_issues_form_details_bottom,
     :partial => 'hooks/edit_attributes'
 
+  # Controller hook to set versions on save
   def controller_issues_edit_before_save(context={ })
     params = context[:params]
     issue = context[:issue]
 
-    if issue && params.include?(:issue) && params[:issue].include?(:releases)
+    if issue && params.include?(:issue) && params[:issue].include?(:found_versions)
       begin
-        issue.releases = [Release.find_by_name(params[:issue][:releases])] 
+        # TODO: add separate permission to *remove* versions, normal users can only append
+        issue.found_version_ids = params[:issue][:found_versions]
       rescue ArgumentError, TypeError
-        issue.remaining_hours = []
+        # change nothing
+      end
+    end
+    if issue && params.include?(:issue) && params[:issue].include?(:fixed_versions)
+      begin
+        # TODO: add separate permission to *remove* versions, normal users can only append
+        issue.fixed_version_ids = params[:issue][:fixed_versions]
+      rescue ArgumentError, TypeError
+        # change nothing
       end
     end
   end
